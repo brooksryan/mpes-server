f019885b279881cc4a81674edbca0cb778b7eaec
Brooks Ryan <brooks@elementum.com>
2018-08-02 20:31:52 +0000

    removing migrations from git, returned server to normal sqlite3

--- mpesapp/settings.py
+++ mpesapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p__!!7lg-vk*t-9#(=txu)wv%)6r4$))+aq^0zw0%h&b&s-cv6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','mpes-brooksryan.c9users.io', '178.128.8.175']
+ALLOWED_HOSTS = ['www.mpes-serve.com', '127.0.0.1','mpes-brooksryan.c9users.io', '178.128.8.175']
 
 
 # Application definition
@@ -86,13 +86,9 @@ WSGI_APPLICATION = 'mpesapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mpesdb',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                    # Set to empty string for default.
     }
 }
 
@@ -136,6 +132,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = '/static/'
 
